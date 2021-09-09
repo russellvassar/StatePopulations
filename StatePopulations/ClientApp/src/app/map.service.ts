21b6a2756f0eeeed815189029ecb73e06a042c3d
Russell Vassar <rvassar@hdlccpropertytax.com>
2021-09-08 19:21:36 -0700

    The state that the mouse is hovering over is now highlighted.

--- StatePopulations/ClientApp/src/app/map.service.ts
+++ StatePopulations/ClientApp/src/app/map.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import TileLayer from 'ol/layer/Tile';
-import { Feature, Map, View } from 'ol';
+import { Feature, Map, MapBrowserEvent, MapEvent, View } from 'ol';
 import { WKT } from 'ol/format';
 import { state } from './state.service';
 import Geometry from 'ol/geom/Geometry';
 import VectorSource from 'ol/source/Vector';
 import { Vector } from 'ol/layer';
 import { XYZ } from 'ol/source';
+import BaseEvent from 'ol/events/Event';
+import Style from 'ol/style/Style';
+import Stroke from 'ol/style/Stroke';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +17,7 @@ import { XYZ } from 'ol/source';
 export class MapService {
   private map?: Map;
   private wktFormat = new WKT();
+  //private hoverState: state = null;
   private stateVector = new Vector({
     source: new VectorSource()
   });
@@ -23,32 +27,51 @@ export class MapService {
   public loadStatesOnMap(states: state[]) {
     const stateFeatures: Feature<any>[] = [];
     let mainMaxX = 0;
-    for (let state of states) {
+    for (let state of states) { //Loop through the states and add features to the map.
       const feature = this.wktFormat.readFeature(state.wkt);
-      feature.set("population", state.population);
-      feature.set("gdp", state.gdpBillionsChained2012);
-      feature.set("name", state.name);
+      //Add population, gdp, and name to each state.
+      feature.set('population', state.population);
+      feature.set('gdp', state.gdpBillionsChained2012);
+      feature.set('name', state.name);
       stateFeatures.push(feature);
-      if (state.name.toLowerCase() === "maine") {
-        mainMaxX = (<Geometry>feature.getGeometry()).getExtent()[2];
+      if (state.name.toLowerCase() === 'maine') {
+        mainMaxX = (<Geometry>feature.getGeometry()).getExtent()[2]; //Save the maxx for Maine.
       }
     }
     this.stateVector.getSource().addFeatures(stateFeatures);
     const extent = this.stateVector.getSource().getExtent();
-    extent[2] = mainMaxX; //Fix the map, so it doesn't wrap around the world.
+    //Fix the map, so it doesn't wrap around the world.
+    //The edge of Alaska wraps from negative to positive longitude, so we'll basically get the whole world if we don't fix it.
+    //Set the right edge the egde of Maine instead.
+    extent[2] = mainMaxX; 
     this.map?.getView().fit(extent);
   }
 
   public initializeMap(mapId: string) {
+    //Set up the map.
+    this.createMap(mapId);
+    //Subscribe to pointermove event.
+    this.map?.on('pointermove', (event: MapBrowserEvent<MouseEvent>) => {
+      this.getHoverFeatures(event.coordinate); //Get the features
+      const hoverFeatures = this.getHoverFeatures(event.coordinate); //Get the features that the mouse is over.
+      this.clearFeatureStyles(); //Clear current styles.
+      if (hoverFeatures && hoverFeatures.length) { //if there are any features under the mouse.
+        this.setHoverStyle(hoverFeatures[0]); //Set the feature under the mouse to the hover style.
+      }
+    });
+  }
+
+  //Private methods.
+  private createMap(mapId: string) {
     this.map = new Map({
       target: mapId,
       layers: [
         new TileLayer({
           source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png' //This is the open street map base layer.
           })
         }),
-        this.stateVector
+        this.stateVector //This is the layer that contains the states.
       ],
       view: new View({
         zoom: 2,
@@ -57,4 +80,22 @@ export class MapService {
     });
   }
 
+  private getHoverFeatures(coordinate: number[]) {
+    return this.stateVector.getSource().getFeaturesAtCoordinate(coordinate);
+  }
+
+  private clearFeatureStyles() {
+    for (const f of this.stateVector.getSource().getFeatures()) {
+      f.setStyle(undefined);
+    }
+  }
+
+  private setHoverStyle(feature: Feature<Geometry>) {
+    feature.setStyle(new Style({
+      stroke: new Stroke({
+        width: 5,
+        color: 'red'
+      })
+    }));
+  }
 }
