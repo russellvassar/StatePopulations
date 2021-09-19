f13c804098164591ddbac656d9ca9db5d54245cb
Russell Vassar <rvassar@hdlccpropertytax.com>
2021-09-18 20:32:57 -0700

    Changed tab size.

--- StatePopulations/ClientApp/src/app/map.service.spec.ts
+++ StatePopulations/ClientApp/src/app/map.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { MapService } from './map.service';
 
 describe('MapService', () => {
-  let service: MapService;
+    let service: MapService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MapService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(MapService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
